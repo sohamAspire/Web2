510a26b06a9c40c9c60092f94a366739bdd55311
Soham Patel <Keval.jesani@aspiresoftserv.com>
2023-02-14 12:08:19 +0530

    Modal Based Commit

--- index.js
+++ index.js
@@ -1,38 +1,53 @@
+const Email = document.getElementById('inputEmail4');
+const Password = document.getElementById('inputPassword4');
+var itemsArray = [];
 
-myForm = document.getElementById('myForm');
-myForm.addEventListener('submit', function (e) {
-    e.preventDefault(); // It will helps to stop reload
-
-    // Targeting all values 
-    const Email = document.getElementById("inputEmail4").value;
-    const Password = document.getElementById("inputPassword4").value;
-    const Address = document.getElementById("inputAddress").value;
-    const City = document.getElementById("inputCity").value;
-    const State = document.getElementById("inputState").value;
-    if (Email == "" || Password == "" || Address == "" || City == "" || State =="") {
-        alert("Please Filled Up the Complete Form")
-    } else {
-        
-    }
-    // Making Object 
-    var Data = {
-        Email: Email,
-        Password: Password,
-        Address: Address,
-        City: City,
-        State: State
+function addData() {
+    
+    userdetail = {
+        Email: Email.value,
+        Password: Password.value
     };
-    console.log(Data);
+    
+    itemsArray.push(userdetail)
+    localStorage.setItem('items', JSON.stringify(itemsArray))
+    data1 = JSON.parse(localStorage.getItem('items'));
+    console.log(data1);
+    
+    // // console.log(localStorage);
+    // localStorage.setItem("Data",userdetail);
+    // console.log(localStorage);
+    // // localStorage.setItem("Password",Password.value);
+    
+    document.getElementById("data").innerHTML += `<tr>
+    <td>${localStorage.getItem("Email")}</td>
+    <td>${localStorage.getItem("Password")}</td>
+    <td><button class='btn btn-outline-secondary' onclick='openMod(this)'>Edit</button>
+    <button class='btn btn-outline-danger' onclick='deleteAl(this)'>Delete</button></td>
+    </tr>
+    `
+}
 
-    // Table Stripes 
-    document.getElementById('demo').innerHTML += `<tr>
-    <td>${Data.Email}</td>
-    <td>${Data.Password}</td>
-    <td>${Data.Address}</td>
-    <td>${Data.City}</td>
-    <td>${Data.State}</td> </tr>`
-})
+// console.log(localStorage);
+function showData() {
+    data1 = JSON.parse(localStorage.getItem('items'));
+    console.log(data1);
+    data1.forEach((i) => {
+        document.getElementById("data").innerHTML += `<tr>
+            <td>${i.Email}</td>
+            <td>${i.Password}</td>
+            <td><button class='btn btn-outline-secondary' data-toggle='modal' data-target='#exampleModalCenter'>Edit</button>
+            <button class='btn btn-outline-danger' onclick='deleteAl(this)'>Delete</button></td>
+            </tr> `
+    })
+}
 
+function deleteAl(e) {
+    e.parentElement.parentElement.remove();
+}
 
-// Edit 
-// Delete and 
+function openMod(e) {
+    data1 = JSON.parse(localStorage.getItem('items'));
+    Email.value = i.Email;
+    Password.value = i.Password;
+}
