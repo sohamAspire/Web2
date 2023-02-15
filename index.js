e031fd99486500a0cf0a0b56620a44352b537b85
Soham Patel <Keval.jesani@aspiresoftserv.com>
2023-02-15 15:50:38 +0530

    "Stable"

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 const Email = document.getElementById('inputEmail4');
 const Password = document.getElementById('inputPassword4');
-const updating = document.getElementById("updating");
-updating.classList.add("disabled");
+
+// localStorage.clear();
 var itemsArray = [];
 
 // Onload 
 function showData() {
-    
+    updating.classList.add("hide")
     data1 = JSON.parse(localStorage.getItem('items'));
     console.log(data1);
-    data1.forEach((i) => {
+    data1.forEach((i , index) => {
         document.getElementById("data").innerHTML += `<tr>
             <td>${i.Email}</td>
             <td>${i.Password}</td>
-            <td><button class='btn btn-outline-secondary' data-toggle='modal' onclick='openMod(this)' data-target='#exampleModalCenter'>Edit</button>
-            <button class='btn btn-outline-danger' onclick='deleteAl(this)'>Delete</button></td>
+            <td><button class='btn btn-outline-secondary' data-toggle='modal' onclick='openMod(this,${index})' data-target='#exampleModalCenter'>Edit</button>
+            <button class='btn btn-outline-danger' onclick='deleteAl(this,${index})'>Delete</button></td>
             </tr> `
     })
 }
@@ -31,40 +31,33 @@ function addData() {
     localStorage.setItem('items', JSON.stringify(itemsArray))
     data1 = JSON.parse(localStorage.getItem('items'));
     console.log(data1);
-    
-
-    // // console.log(localStorage);
-    // localStorage.setItem("Data",userdetail);
-    // console.log(localStorage);
-    // // localStorage.setItem("Password",Password.value);
-
-    document.getElementById("data").innerHTML += `<tr>
-    <td>${Email.value}</td>
-    <td>${Password.value}</td>
-    <td><button class='btn btn-outline-secondary' onclick='openMod(this)' data-toggle='modal' data-target='#exampleModalCenter'>Edit</button>
-    <button class='btn btn-outline-danger' onclick='deleteAl(this)'>Delete</button></td>
-    </tr>
-    `
 }
 
 // Add Data 
 
 
 // Delete Data 
-function deleteAl(e) {
-    e.parentElement.parentElement.remove();
-    var data = JSON.parse(localStorage.getItem("items"));
-    data.splice(e, 1);
-    localStorage.setItem('items',JSON.stringify(data))
-    e.parentElement.parentElement.remove();
+function deleteAl(e ,ind) {
+   data = JSON.parse(localStorage.getItem('items'));
+   data.splice(ind, 1)
+   localStorage.setItem('items',JSON.stringify(data))
+   e.parentElement.parentElement.remove();
 }
 // Delete Data 
 
 // Edit 
-function openMod(e) {
-    
+function openMod(e , id) {
+    updating.classList.remove("hide")
     Email.value = e.parentElement.parentElement.cells[0].innerHTML;
     Password.value = e.parentElement.parentElement.cells[1].innerHTML;
-    return updating.classList.remove("disabled");
+    btn = document.getElementById("updating");
+    data = JSON.parse(localStorage.getItem('items'));
+    btn.addEventListener("click", function () {
+       data[id] = {
+            Email : Email.value,
+            Password: Password.value
+       }
+       console.log(data);
+       localStorage.setItem('items',JSON.stringify(data))
+    })
 }
-// Edit 
